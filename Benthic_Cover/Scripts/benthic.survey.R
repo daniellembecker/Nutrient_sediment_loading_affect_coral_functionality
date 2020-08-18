a21e796e7f7f7aba02eb2a055a4c1762becd149a
Danielle Becker <danielle.becker.927@my.csun.edu>
2020-08-18 12:15:02 -0700

    changes to benthic cover, added site letters to plots

--- Benthic_Cover/Scripts/benthic.survey.R
+++ Benthic_Cover/Scripts/benthic.survey.R
@@ -1,6 +1,7 @@
 ##Processing benthic survey data for Summer 2019
 #Created by: Danielle Becker
 #Created on: 20191122
+#Edited on: 20200818
 
 #clear dataframe
 rm(list=ls())
@@ -16,7 +17,7 @@ library(here)
 here()
 
 #load in meta data sheet
-benthic.dat <- read_csv("Summer_2019/benthic.cover/Data/benthic.survey.csv")
+benthic.dat <- read_csv("Benthic_Cover/Data/benthic.survey.csv")
 view(benthic.dat)
 
 #make all NA values in the data sheet read as 0's
@@ -46,7 +47,7 @@ benthic.dat.cal$site.block  <- c(1, 3, 2, 2, 1, 3)
 benthic.dat.cal$site  <- c("A", "B", "A", "B", "B", "A")
 
 #load in the environmental data to compare p. acuta along a nutrient gradient
-nut.dat <- read.csv("Summer_2019/parameter.connections/Data/covariate.data.csv")
+nut.dat <- read.csv("Effect_Sizes/Data/covariate.data.csv")
 
 #combine nut.dat and benthic cover
 tot.dat <- left_join(benthic.dat.cal, nut.dat)
@@ -62,13 +63,14 @@ qqp(tot.dat$percent.cover, "norm")
 ggplot(tot.dat, aes(x=N.resid, y=percent.cover)) + #set aesthetics for my x and y usuing means for x and y vars
   geom_point() +
   theme_bw()+ #overall theme for plot
+  geom_text(data = tot.dat, aes(label = site.letter), vjust = 0.9, hjust = 2.0) + #add labels for each site 
   xlab(expression(bold("Percent Nitrogen (%)"))) + ylab(expression(bold(~bolditalic(Pocillopora~acuta)~ "Percent Cover"))) +
   theme(plot.title = element_text(face = "bold", color = "black", size = 14, hjust = 0.5)) + #adjust themes for plot title
   theme(axis.text.x=element_text(face="bold", color="black", size=12), axis.text.y=element_text(face="bold", color="black", size=12), axis.title.x = element_text(face="bold", color="black", size=14), axis.title.y = element_text(face="bold", color="black", size=14),panel.grid.major=element_blank(), panel.grid.minor=element_blank()) + #adjust themes for chart x and y axis labels and axis tick mark labels
   geom_smooth(data=tot.dat, aes(x=N.resid, y=percent.cover), method = "lm") + #add lm relationship with raw data sheet, filter out for rate type represented 
   xlim(-0.095, 0.095)
 
-ggsave(filename = "Summer_2019/benthic.cover/Output/percent.cover.p.acuta.png", device = "png", width = 7, height = 5)
+ggsave(filename = "Benthic_Cover/Output/percent.cover.p.acuta.pdf", device = "pdf", width = 7, height = 5)
 
 #create linear model to test relationship
 percent.cover.lm = lm(percent.cover ~ N.resid, data=tot.dat)
@@ -88,6 +90,7 @@ qqp(tot.dat$percent.cover, "norm")
 #look at relationship of %N to p.acuta percent cover
 ggplot(tot.dat, aes(x=trap.resid, y=percent.cover)) + #set aesthetics for my x and y usuing means for x and y vars
   geom_point() +
+  geom_text(data = tot.dat, aes(label = site.letter), vjust = 0.5, hjust = 2.0) + #add labels for each site 
   theme_bw()+ #overall theme for plot
   xlab(expression(bold("Sedimentation Rate (" *mg *~cm^-2~day^-1*")"))) +  ylab(expression(bold(~bolditalic(Pocillopora~acuta)~ "Percent Cover"))) +
   theme(plot.title = element_text(face = "bold", color = "black", size = 16, hjust = 0.5)) + #adjust themes for plot title
@@ -95,7 +98,7 @@ ggplot(tot.dat, aes(x=trap.resid, y=percent.cover)) + #set aesthetics for my x a
   geom_smooth(data=tot.dat, aes(x=trap.resid, y=percent.cover), method = "lm")  #add lm relationship with raw data sheet, filter out for rate type represented 
 
 
-ggsave(filename = "Summer_2019/benthic.cover/Output/trap.resid.percent.cover.p.acuta.png", device = "png", width = 7, height = 5)
+ggsave(filename = "Benthic_Cover/Output/trap.percent.cover.pdf", device = "f", width = 7, height = 5)
 
 #create linear model to test relationship
 percent.cover.lm = lm(percent.cover ~ trap.resid, data=tot.dat)
@@ -103,7 +106,7 @@ percent.cover.lm = lm(percent.cover ~ trap.resid, data=tot.dat)
 #calculate the r squared and p value
 summary(percent.cover.lm) 
 
-write.csv(tot.dat, file = "Summer_2019/benthic.cover/Data/percent.cover.csv")
+write.csv(tot.dat, file = "Benthic_Cover/Data/percent.cover.csv")
 
 
 
